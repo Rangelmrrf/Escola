ad7270757f7a4e08a429095d21fa33e7f82f75be
Marcos <fp01ajweb8@vm-java.season.local>
2017-05-04 22:32:13 -0300

    Aula 04/05

---
+++ src/main/java/rangelmrrf/escola/dao/TurmaDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rangelmrrf.escola.dao;
+
+import rangelmrrf.escola.model.Turma;
+
+/**
+ *
+ * @author fp01ajweb8
+ */
+public class TurmaDAO extends GenericDAO<Turma>{
+    
+}
